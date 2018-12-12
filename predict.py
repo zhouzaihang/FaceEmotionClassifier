07fcd7c28ae692b4f6d2c98e7971b6cc0540b838
Zaihang <1085212484@qq.com>
2018-12-12 12:32:22 +0800

    完成识别和emoji显示
    下一步重构代码 写文档

--- predict.py
+++ predict.py
@@ -16,54 +16,78 @@ model = model_from_json(loaded_model_json)
 # 加载模型权重
 model.load_weights(model_path + 'model_weight.h5')
 
-if __name__ == '__main__':
-    # 创建VideoCapture对象
-    capture = cv2.VideoCapture(0)
+# 加载emotion
+emotion_images = {}
+for emoji in emotion_labels:
+    emotion_images[emoji] = cv2.imread("./emoji/" + emoji + ".png", -1)
 
-    # 使用opencv的人脸分类器
-    cascade = cv2.CascadeClassifier(model_path + 'haarcascade_frontalface_alt.xml')
 
-    while True:
-        ret, frame = capture.read()
+def face2emoji(face, emotion_index, position):
+    x, y, w, h = position
+    emotion_image = cv2.resize(emotion_images[emotion_index], (w, h))
+    overlay_img = emotion_image[:, :, :3]/255.0
+    overlay_bg = emotion_image[:, :, 3:]/255.0
+    background = (1.0 - overlay_bg)
+    face_part = (face[y:y + h, x:x + w]/255.0) * background
+    overlay_part = overlay_img * overlay_bg
 
-        # 灰度化处理
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face[y:y + h, x:x + w] = (cv2.addWeighted(face_part, 255.0, overlay_part, 255.0, 0.0))
 
-        # 识别人脸位置
-        faceLands = cascade.detectMultiScale(gray, scaleFactor=1.1,
-                                             minNeighbors=1, minSize=(120, 120))
+    return face
 
-        if len(faceLands) > 0:
-            for faceLand in faceLands:
-                x, y, w, h = faceLand
-                images = []
-                result = np.array([0.0] * num_class)
 
-                # 裁剪出脸部图像
-                image = cv2.resize(gray[y:y + h, x:x + w], (img_size, img_size))
-                image = image / 255.0
-                images.append(image)
-                images.append(cv2.flip(image, 1))
-                images.append(cv2.resize(image[2:45, :], (img_size, img_size)))
+# 创建VideoCapture对象
+capture = cv2.VideoCapture(0)
 
-                for image in images:
-                    image = image.reshape(1, img_size, img_size, 1)
-                    predict_lists = model.predict_proba(image, batch_size=32, verbose=1)
-                    # print(predict_lists)
-                    result += np.array([predict for predict_list in predict_lists
-                                        for predict in predict_list])
-                    # print(result)
-                    emotion = emotion_labels[int(np.argmax(result))]
-                    print("Emotion:", emotion)
-                    cv2.rectangle(frame, (x - 20, y - 20), (x + w + 20, y + h + 20),
-                                  (0, 0, 255), thickness=2)
-                    cv2.putText(frame, '%s' % emotion, (x + 30, y + 30),
-                                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 4)
+# 使用opencv的人脸分类器
+cascade = cv2.CascadeClassifier(model_path + 'haarcascade_frontalface_alt.xml')
 
-            cv2.imshow('EmotionClassify', frame)
-            if cv2.waitKey(60) == ord('q'):
-                break
+while True:
+    ret, frame = capture.read()
 
-    # 释放摄像头并销毁所有窗口
-    capture.release()
-    cv2.destroyAllWindows()
+    # 灰度化处理
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # 呈现用emoji替代后的画面
+    emoji_show = frame.copy()
+
+    # 识别人脸位置
+    faceLands = cascade.detectMultiScale(gray, scaleFactor=1.1,
+                                         minNeighbors=1, minSize=(120, 120))
+
+    if len(faceLands) > 0:
+        for faceLand in faceLands:
+            x, y, w, h = faceLand
+            images = []
+            result = np.array([0.0] * num_class)
+
+            # 裁剪出脸部图像
+            image = cv2.resize(gray[y:y + h, x:x + w], (img_size, img_size))
+            image = image / 255.0
+            image = image.reshape(1, img_size, img_size, 1)
+
+            # 调用模型预测情绪
+            predict_lists = model.predict_proba(image, batch_size=32, verbose=1)
+            # print(predict_lists)
+            result += np.array([predict for predict_list in predict_lists
+                                for predict in predict_list])
+            # print(result)
+            emotion = emotion_labels[int(np.argmax(result))]
+            print("Emotion:", emotion)
+
+            emoji = face2emoji(emoji_show, emotion, (x, y, w, h))
+            cv2.imshow("Emotion", emoji)
+
+            # 框出脸部并且写上标签
+            cv2.rectangle(frame, (x - 20, y - 20), (x + w + 20, y + h + 20),
+                          (0, 255, 255), thickness=10)
+            cv2.putText(frame, '%s' % emotion, (x, y - 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 4)
+            cv2.imshow('Face', frame)
+
+        if cv2.waitKey(60) == ord('q'):
+            break
+
+# 释放摄像头并销毁所有窗口
+capture.release()
+cv2.destroyAllWindows()
